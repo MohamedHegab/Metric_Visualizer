dab75b90be027c8194218a87ac887b2954e07611
Hegab <mohamed.hegab@zyda.com>
2021-06-01 10:10:34 +0200

    add tailwind and nivo libraries

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Metrics from './Metrics/Metrics'
 import Metric from './Metric/Metric'
+import "tailwindcss/tailwind.css"
 
 const App = () => {
   return (
