2f6dd2f10f86ce4a8c1e791bb9e519aa2ff78878
Hegab <mohamed.hegab@zyda.com>
2021-06-04 02:42:20 +0200

    Add metric button and form

---
+++ app/javascript/components/AddMetricForm.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const AddMetricForm = ({ closeModal, setAddedMetric }) => {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ name: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        axios
+          .post(`/api/v1/metrics`, {
+            metric: { ...values },
+          })
+          .then((resp) => {
+            closeModal();
+            setAddedMetric(true);
+          })
+          .catch((resp) => console.log(resp));
+        setTimeout(() => {
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <div className="flex bg-gray-bg1">
+          <div className="w-full m-auto">
+            <h1 className="text-2xl font-medium text-primary mb-12 text-center">
+              Add new metric
+            </h1>
+
+            <Form>
+              <label htmlFor="name">Name</label>
+              <Field
+                type="text"
+                name="name"
+                className="w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-500 text-xs italic mb-4"
+              />
+
+              <div className="flex flex-row">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded-full cursor-pointer"
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="text-blue border font-bold px-4 rounded-full cursor-pointer"
+                  onClick={() => closeModal()}
+                >
+                  close
+                </button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      )}
+    </Formik>
+  );
+};
+
+export default AddMetricForm;
--- app/javascript/components/Metrics.js
+++ app/javascript/components/Metrics.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import Metric from "./Metric";
+import AddMetricForm from "./AddMetricForm";
 import { deserialize } from "deserialize-json-api";
 import { ModalProvider } from "react-simple-hook-modal";
+import { Modal, useModal, ModalTransition } from "react-simple-hook-modal";
 
 const Metrics = () => {
   const [metrics, setMetrics] = useState([]);
+  const { isModalOpen, openModal, closeModal } = useModal();
+  const [addedMetric, setAddedMetric] = useState(false);
 
   useEffect(() => {
     axios
       .get("/api/v1/metrics")
       .then((resp) => {
         setMetrics(deserialize(resp.data)?.data);
+        setAddedMetric(false)
       })
       .catch((resp) => console.log(resp));
-  }, [metrics.length]);
+  }, [addedMetric]);
 
   const list = metrics.map((metric) => {
     return <Metric key={metric.id} metric={metric} />;
@@ -22,10 +27,27 @@ const Metrics = () => {
 
   return (
     <ModalProvider>
+      <Modal
+        id="any-unique-identifier"
+        isOpen={isModalOpen}
+        transition={ModalTransition.BOTTOM_UP}
+        modalClassName="min-h-0"
+      >
+        <AddMetricForm
+          closeModal={closeModal}
+          setAddedMetric={setAddedMetric}
+        />
+      </Modal>
       <div className="main-content flex-1 bg-gray-100 pb-24 md:pb-5">
         <div className="bg-gray-800">
-          <div className="bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
+          <div className="bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white flex justify-between">
             <h3 className="font-bold pl-2">Metrics Visualizer</h3>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded-full cursor-pointer"
+              onClick={openModal}
+            >
+              Add Metric
+            </button>
           </div>
         </div>
 
